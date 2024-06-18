36b1bc8b238065c90026e60c6188734ca0e3105a
Dheeraj Bandaru <dbandaru@microsoft.com>
2024-06-17 22:43:42 -0500

    fixing some product page details

--- pages/product/1.js
+++ pages/product/1.js
@@ -2,28 +2,15 @@
 
 import React from 'react';
 
-export async function getStaticProps() {
-    // Mock fetch product data
-    const product = {
-        id: 1,
-        name: `Azure Static Web Apps`,
-        price: 100,
-    };
-
-    return { props: { product } };
-}
-
 export default function Product({ product }) {
     // Render product on the page
     return (
         <div>
             <h1>Product Detail</h1>
-            {product && (
-                <div>
-                    <h2>{product.name}</h2>
-                    <p>{product.price}</p>
-                </div>
-            )}
+            <div>
+                <h2>{`Azure Static Web Apps`}</h2>
+                <p>{100}</p>
+            </div>
         </div>
     );
 }
--- pages/product/2.js
+++ pages/product/2.js
@@ -2,28 +2,15 @@
 
 import React from 'react';
 
-export async function getStaticProps() {
-    // Mock fetch product data
-    const product = {
-        id: 2,
-        name: `Azure Functions`,
-        price: 200,
-    };
-
-    return { props: { product } };
-}
-
 export default function Product({ product }) {
     // Render product on the page
     return (
         <div>
             <h1>Product Detail</h1>
-            {product && (
-                <div>
-                    <h2>{product.name}</h2>
-                    <p>{product.price}</p>
-                </div>
-            )}
+            <div>
+                <h2>{`Azure Functions`}</h2>
+                <p>{200}</p>
+            </div>
         </div>
     );
 }
